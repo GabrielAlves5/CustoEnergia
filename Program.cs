f00f761e9a37ec7fa6a2569e8458c026791a035f
GabrielAlves5 <ecgabriel.alves@gmail.com>
2025-04-07 13:13:23 -0300

    calculo funcionando

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double  tempoDeUso, consumoPorMes, custoluz;
+Console.WriteLine("Calculo de Consumo");
+Console.Write("Consumo do aparelho (em kWh/mês)...: ");
+consumoPorMes = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nHoras de uso por dia...............: ");
+tempoDeUso = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nCusto da energia (em R$/kWh).......: ");
+custoluz = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Custo estimado: R${tempoDeUso * consumoPorMes * custoluz}");
+
+
